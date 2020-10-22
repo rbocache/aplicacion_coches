8379372f44d5ae00f1f681b11c8c24d8ea08597d
rbocache <raulbocache@protonmail.com>
2020-10-22 19:09:09 +0200

    datos coche

---
+++ templates/listado_coches.php
@@ -0,0 +1,18 @@
+<div class="caja-coche">
+            <div class="caja-iz">
+                <img src="<?php echo $propiedad["path"]; ?>" alt="imagen Coche">
+            </div>
+            <div class="caja-dcha">
+                <div class="caja-contenido">
+                        <div class="datos-comp-vent">
+                        <p>Vendedor: <?php echo $coche['anio_fabricacion'] ?> </p>
+                        <p>Comprador: <?php echo $coche['comprador'] ?> </p>
+                        <p>Fecha de compra: <?php echo $coche['fecha_compra'] ?> </p>
+                        </div>
+                        <div class="caja-marca">
+                        </div>
+                        <div class="caja-vehiculo">
+                        </div>
+                </div>                
+            </div>
+</div> <!-- coches -->
