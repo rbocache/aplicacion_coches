8a0bd8efb1d38e153be16e122c6fb66fe03d30e2
rbocache <raulbocache@protonmail.com>
2020-10-23 17:51:04 +0200

    mas templates

--- marca_modelo.php
+++ marca_modelo.php
@@ -6,12 +6,14 @@ $title = "Añadir Datos de Marca, Modelo y Tipo de motor";
 include "./templates/header.php"; ?>
 
 <div class="caja-contenedor">
-    <div class="caja-encabezado">
+    <div class="caja-encabezado ">
         <h1><?php echo $title ?></h1>
+        <div class="botones">
           <a id="cerrar" class="button" href="./index.php">Volver</a>
           <a id="cerrar" class="button" href="./anadir_marca.php">Nueva Marca</a>
           <a id="cerrar" class="button" href="./anadir_modelo.php">Nuevo Modelo</a>
           <a id="cerrar" class="button" href="./anadir_motor">Nuevo Tipo de Motor</a>
+        </div>
     </div>        
     <div class="caja-formulario">  
         <?php
